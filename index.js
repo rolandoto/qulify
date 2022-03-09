6ac5bccfcf38f1a65419df0d62cf7a9d64d21a68
Rolando Guerrero <rolando22_@outlook.com>
2022-03-09 14:42:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const cors = require('cors')
 const AuthRouter = require('./Routes/Routes')
 const { dbConnection } = require('./database/database')
 require('dotenv').config()
+const path = require("path")
 const app =express()
 
 let port = process.env.PORT || 8080;
-
-app.use('/public',express.static(`${__dirname}/Storage/Imgs`))
+app.use(express.static(path.join(__dirname,'/Storage/Imgs')));
 app.use(express.json())
 app.use(cors())
 
